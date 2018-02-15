8c727244614a859f860f4dd3d368702d583d3255
Vermillion4 <gguilber@gmail.com>
2018-02-15 17:23:19 +0100

    fonctionnement, ajout serfvice non utilisée

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UnCollegueComponent } from './un-collegue/un-collegue.component';
-
+import { Collegue } from './shared/domain/collegue';
+import { ListeComponent } from './collegues/liste/liste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UnCollegueComponent
+    UnCollegueComponent,
+    ListeComponent,
   ],
   imports: [
     BrowserModule,
-	NgbModule.forRoot()
+    HttpClientModule,
+	  NgbModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/domain/collegue.ts
+++ src/app/shared/domain/collegue.ts
@@ -1,19 +1,23 @@
 export class Collegue {
 	
-	private _nom:string="Pseudo";
-	private _urlImg:string="URL Image";
-	private _note:number=50;
-	constructor(nom:string,urlImg:string,note:number){
-		this._nom=nom;
-		this._urlImg=urlImg;
-		this._note=note;
+	private _nom = "Pseudo";
+	private _urlImg = "URL Image";
+	private _note = 50;
+
+	constructor(nom:string, urlImg:string, note:number){
+		this._nom = nom;
+		this._urlImg = urlImg;
+		this._note = note;
 	}
+
 	get nom(){
 		return this._nom;
 	}
+
 	get urlImg(){
 		return this._urlImg;
 	}
+
 	get note(){
 		return this._note;
 	}
